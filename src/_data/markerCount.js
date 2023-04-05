63933f108f5d962b3f095b74e3ef4f446efbcbd7
Austin Webre <austin@thewebres.com>
2023-04-05 00:26:43 -0500

    pipeline check

--- src/_data/markerCount.js
+++ src/_data/markerCount.js
@@ -2,6 +2,7 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 require("dotenv").config();
 
 module.exports = async function () {
+  console.log(process.env.API_URL);
   const url = process.env.API_URL + "api/markers/count";
 
   /* This returns a promise */
